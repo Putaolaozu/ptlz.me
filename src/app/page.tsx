ee37ad405e94736cfcc0dd127bc3a56e5e6b87a7
Putaolaozu <liweixuan599@gmail.com>
2023-09-14 15:23:03 +0800

    style(main page): fix UI bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import articlesInfo from "@/articles/articlesInfo.json";
 export default function Home() {
   return (
     <>
-      <div className="sticky top-0 sm:h-16 h-12 w-full z-10 dark:bg-[#0e141a] bg-inherit"></div>
+      <div className="sticky top-0 sm:h-16 h-12 w-full z-10 bg-inherit"></div>
       <section className="flex flex-col gap-4 justify-center max-w-[1100px] mx-auto px-2">
         {articlesInfo.map((articleInfo, index) => {
           return <ArticleCard article={articleInfo} key={index}></ArticleCard>;
